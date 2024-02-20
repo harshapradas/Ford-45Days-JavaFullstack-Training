56b6ca30d23940a916d8180feacaafca9e19e09c
harshapradas <159467518+harshapradas@users.noreply.github.com>
2024-02-20 20:43:59 +0530

    date and time1.java

---
+++ date and time1.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+class DateAndTime{
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now());
+        System.out.println(LocalDateTime.now());
+        System.out.println(LocalTime.now());
+    }
+}
